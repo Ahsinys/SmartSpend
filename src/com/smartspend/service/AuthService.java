df6648e8186961c71f65f70c43839650c18c99b8
Ahsinys <ahsinyounas05@gmail.com>
2026-06-08 20:01:31 +0500

    Day 3: AuthService with login and register logic added

--- src/com/smartspend/service/AuthService.java
+++ src/com/smartspend/service/AuthService.java
@@ -1,4 +1,70 @@
 package com.smartspend.service;
 
+import com.smartspend.filehandler.UserFileHandler;
+import com.smartspend.model.User;
+import com.smartspend.util.FileManager;
+
 public class AuthService {
+
+    private UserFileHandler userFileHandler;
+
+    public AuthService() {
+        userFileHandler = new UserFileHandler("data/users.csv");
+    }
+
+    // LOGIN
+    public User login(String username, String password) {
+
+        if (!isValidInput(username, password)) {
+            return null;
+        }
+
+        return userFileHandler.login(username, password);
+    }
+
+    // REGISTER
+    public boolean register(String username,
+                            String password,
+                            String email) {
+
+        if (!isValidInput(username, password)) {
+            return false;
+        }
+
+        if (userFileHandler.usernameExists(username)) {
+            return false;
+        }
+
+        int id = FileManager.getNextId("data/users.csv");
+
+        User user = new User(
+                id,
+                username,
+                password,
+                email
+        );
+
+        userFileHandler.add(user);
+
+        return true;
+    }
+
+    // VALIDATION
+    private boolean isValidInput(String username,
+                                 String password) {
+
+        if (username == null || username.trim().isEmpty()) {
+            return false;
+        }
+
+        if (password == null || password.trim().isEmpty()) {
+            return false;
+        }
+
+        if (password.length() < 6) {
+            return false;
+        }
+
+        return true;
+    }
 }
